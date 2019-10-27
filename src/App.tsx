9c81b80011a1158ea9a0fe54fa89e2bda80c1d13
demente <olga.agen@gmail.com>
2019-10-27 12:58:45 +0100

    login with spotify

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { ReactPropTypes } from 'react';
 import Login from './components/Login';
 
 import ResponsiveContainer from './components/ResponsiveContainer';
 import Footer from './components/Footer';
+import SearchResult from './components/SearchResult';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <ResponsiveContainer>
-      <Login />
-      {/* <SearchResult/> */}
 
-      <Footer />
-    </ResponsiveContainer>
-  );
+type HashParams = {
+  access_token?: string;
+  token_type?: string;
+  expires_in?: number;
+  state?: string;
+}
+
+
+let params: HashParams = {};
+window.location.hash
+  .substring(1)
+  .split("&").forEach((item) => {
+    const parts: string[] = item.split("=");
+    if (parts[0] === 'access_token') {
+      params.access_token = parts[1];
+    }
+  })
+
+window.location.hash = "";
+
+type OwnState = { token?: string };
+type OwnProps = {};
+
+class App extends React.Component<OwnProps, OwnState>  {
+
+  componentDidMount() {
+    let _token: string | undefined = params.access_token;
+
+    if (_token) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${_token}`;
+      this.setState({
+        token: _token
+      });
+    }
+  }
+
+  render() {
+    return (
+      <ResponsiveContainer>
+        {(!this.state || !this.state.token)
+          && (<Login />)}
+        {this.state && this.state.token && <SearchResult />}
+
+        <Footer />
+      </ResponsiveContainer>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,13 +6,26 @@ import {
 } from 'semantic-ui-react'
 
 
-const height = window.innerWidth;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize/';
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const redirectUri = process.env.REACT_APP_REDIRECT_URL;
+const scopes = [
+  "user-library-read",
+  "playlist-read-collaborative",
+  "playlist-read-private"
+];
+
+
 class Login extends React.Component {
 
   render() {
     return <Grid columns={1} container textAlign='center'>
       <Grid.Row style={{ marginTop: '20%', marginBottom: '20%' }}>
-        <Button primary size='huge'>Login with Spotify</Button>
+        <Button primary size='huge'
+         href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
+          Login with Spotify
+          </Button>
       </Grid.Row>
     </Grid>;
   }
